e2d404c1a1ef0b5b76aabfcc53989a2a7e55a33f
cmiller <lordsauronthegreat@gmail.com>
2010-07-31 07:41:44 +0800

    Larger picture, still no scaling problem.

--- Classes/MyGameDelegate.m
+++ Classes/MyGameDelegate.m
@@ -24,7 +24,7 @@
 
 - (void)gameDidLoad
 {
-    image = [[GBImage alloc] initWithURL:[NSURL fileURLWithPath:@"/Library/User Pictures/Nature/Lightning.tif"]];
+    image = [[GBImage alloc] initWithURL:[NSURL fileURLWithPath:@"/Library/Desktop Pictures/Classic Aqua Blue.jpg"]];
     if (!image) abort();
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_RECTANGLE_ARB, texture);
